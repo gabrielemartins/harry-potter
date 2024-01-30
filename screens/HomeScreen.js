6fc88e10180182fdce75e56ed1b1ca1dda9c68ab
gabrielemartins <gabriieele6@gmail.com>
2024-01-30 08:22:13 -0300

    feat: add more cards to the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,41 @@
 import { View, StyleSheet } from 'react-native';
 import MenuOptionCard from '../components/MenuOptionCard';
 
-const characters = {
-    image: require('../assets/characters-background.jpg'),
-    title: 'Characters',
-    subtitle: 'List of all characters in the Harry Potter World.',
-}
-
 export default function HomeScreen() {
+    const screens = {
+        characters: {
+            image: require('../assets/characters-background.jpg'),
+            title: 'Characters',
+            subtitle: 'List of all characters in the Harry Potter World.',
+        },
+        spells: {
+            image: require('../assets/spells-background.jpg'),
+            title: 'Spells',
+            subtitle: 'List of all spells in the Harry Potter World.',
+        },
+        potions: {
+            image: require('../assets/potions-background.jpg'),
+            title: 'Potions',
+            subtitle: 'List of all potions in the Harry Potter World.',
+        }
+    };
+
     return (
         <View style={styles.container}>
             <MenuOptionCard
-                image={characters.image}
-                title={characters.title}
-                subtitle={characters.subtitle}
+                image={screens.spells.image}
+                title={screens.spells.title}
+                subtitle={screens.spells.subtitle}
+            />
+            <MenuOptionCard
+                image={screens.characters.image}
+                title={screens.characters.title}
+                subtitle={screens.characters.subtitle}
+            />
+            <MenuOptionCard
+                image={screens.potions.image}
+                title={screens.potions.title}
+                subtitle={screens.potions.subtitle}
             />
         </View>
     );
@@ -22,5 +44,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
     },
 });
