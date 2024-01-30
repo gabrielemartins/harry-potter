d164db76ad610c7210c9c0bf26490b7ae625db4b
gabrielemartins <gabriieele6@gmail.com>
2024-01-30 17:40:43 -0300

    feat: center cards on the screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -22,7 +22,7 @@ export default function HomeScreen() {
 
     return (
         <View style={styles.container}>
-            <ScrollView horizontal={true}>
+            <ScrollView horizontal={true} contentContainerStyle={styles.menuCards}>
                 <MenuOptionCard
                     image={screens.characters.image}
                     title={screens.characters.title}
@@ -46,6 +46,10 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    menuCards: {
+        flex: 1,
+        alignItems: 'center',
         flexDirection: 'row',
     },
 });
