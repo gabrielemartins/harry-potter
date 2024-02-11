c01422c40c3cf7631913897b6f943b8912368b44
gabrielemartins <gabriieele6@gmail.com>
2024-02-11 11:04:23 -0300

    feat(Api.js): get spells from the api

--- Api.js
+++ Api.js
@@ -8,3 +8,12 @@ export const getCharacters = async (pageNumber) => {
     }
     return data.data;
 }
+
+export const getSpells = async (pageNumber) => {
+    const response = await fetch(api + 'spells?page[number]=' + pageNumber + '&page[size]=100');
+    const data = await response.json();
+    if (response.status !== 200) {
+        throw new Error(data.errors ? data.errors[0].detail : 'Unknown error occurred');
+    }
+    return data.data;
+}
