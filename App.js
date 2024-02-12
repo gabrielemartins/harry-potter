bfdfaa52678f62393d8f2d07b214b2dad97d3708
gabrielemartins <gabriieele6@gmail.com>
2024-02-12 11:06:13 -0300

    feat(App.js): create the navigation for the app

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import CharactersScreen from './screens/CharactersScreen';
+import SpellsScreen from './screens/SpellsScreen';
 import PotionsScreen from './screens/PotionsScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <PotionsScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerTransparent: true, title:''}} />
+        <Stack.Screen name="Characters" component={CharactersScreen} />
+        <Stack.Screen name="Spells" component={SpellsScreen} />
+        <Stack.Screen name="Potions" component={PotionsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
