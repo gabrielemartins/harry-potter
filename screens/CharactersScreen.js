ec9686b796177ffdcfb214a8a5d4c8d898e2ed48
gabrielemartins <gabriieele6@gmail.com>
2024-02-12 11:35:02 -0300

    feat(CharactersScreen): change the border color of the card

--- screens/CharactersScreen.js
+++ screens/CharactersScreen.js
@@ -117,7 +117,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        borderBottomColor: '#261243',
+        borderBottomColor: '#f2f2f2',
         borderBottomWidth: 1,
         padding: 25,
     },
