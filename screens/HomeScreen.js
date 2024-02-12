54b038cf02fc5e3f9f013065daf471fe90dce764
gabrielemartins <gabriieele6@gmail.com>
2024-02-12 11:32:46 -0300

    feat(HomeScreen.js): change the margin of the components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     header: {
-        marginTop: 90,
+        marginTop: 70,
         marginHorizontal: 20,
     },
     pageTitle: {
@@ -96,12 +96,12 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
     menuCards: {
-        marginTop: 20,
+        
         marginLeft: 5,
     },
     cardContainer: {
         width: CARD_WIDTH,
-        height: '80%',
+        height: '85%',
         marginTop: 40,
     },
 });
